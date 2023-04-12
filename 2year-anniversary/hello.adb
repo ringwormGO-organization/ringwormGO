81509aef2ed8364caad3de8da32676befda4a67f
Stjepan Bilić Matišić <stjepan.bilicmatsic546@gmail.com>
2023-04-12 09:38:49 +0200

    2 year anniversary of ringwormGO
    by @Andrej123456789

---
+++ 2year-anniversary/hello.adb
@@ -0,0 +1,9 @@
+-- To compile this program,
+-- 1. install gcc-ada
+-- 2. run gnatmake hello.adb
+
+with Text_IO; use Text_IO;
+procedure hello is
+begin
+   Put_Line("Happy 2nd anniversary, ringwormGO!!!!!");
+end hello;
